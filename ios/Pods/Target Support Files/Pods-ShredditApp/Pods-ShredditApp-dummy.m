40c489e0df379ce8bed87e1eb0528abdcf4d7eb4
michaelsoffline <cuberomichael13@gmail.com>
2022-03-30 06:08:29 -0400

    Updated project files for MongoDB
    Replaced previous iteration of project files with updated files no longer managed by Expo. Should allow the app to integrate easier with MongoDB

---
+++ ios/Pods/Target Support Files/Pods-ShredditApp/Pods-ShredditApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShredditApp : NSObject
+@end
+@implementation PodsDummy_Pods_ShredditApp
+@end
---
+++ realmApp.js
@@ -0,0 +1,5 @@
+import Realm from "realm";
+
+const app = new Realm.App({ id: "<your Realm app ID here>" });
+
+export default app;
