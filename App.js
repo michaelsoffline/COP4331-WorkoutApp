9f0f915cd8a062180970e93b5d99b6e6e6919989
michaelsoffline <cuberomichael13@gmail.com>
2022-03-29 21:38:07 -0400

    Added changes to navbar
    Made changes to Navbar so that they match Chase's design

--- App.js
+++ App.js
@@ -5,16 +5,43 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "./screens/WelcomeScreen.js";
 import LoginScreen from "./screens/LoginScreen.js";
 import RegisterScreen from "./screens/RegisterScreen.js";
+import ProfileScreen from "./screens/ProfileScreen.js";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 const RootStack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <RootStack.Navigator initialRouteName="Welcome">
+      <RootStack.Navigator initialRouteName="Register">
         <RootStack.Screen name="Welcome" component={WelcomeScreen} />
-        <RootStack.Screen name="Register" component={RegisterScreen} />
-        <RootStack.Screen name="Login" component={LoginScreen} />
+        <RootStack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{
+            title: "Register",
+            headerStyle: { backgroundColor: "#4FB5D3" },
+            headerTitleStyle: {
+              color: "#fff",
+              fontWeight: "bold",
+              fontSize: 30,
+            },
+          }}
+        />
+        <RootStack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            title: "Login",
+            headerStyle: { backgroundColor: "#ACD1AF" },
+            headerTitleStyle: {
+              color: "#fff",
+              fontWeight: "bold",
+              fontSize: 30,
+            },
+          }}
+        />
+        <RootStack.Screen name="Profile" component={ProfileScreen} />
       </RootStack.Navigator>
     </NavigationContainer>
   );
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,137 +1,146 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import react from 'react-native'
-import { StyleSheet, Text, View, Image, ImageBackground, Button, TextInput} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import react from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  Button,
+  TextInput,
+} from "react-native";
 
-import { Alert, SafeAreaView, TouchableWithoutFeedback } from 'react-native-web';
+import {
+  Alert,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+} from "react-native-web";
 
 function RegisterScreen(props) {
-
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [firstName, setFirst] = useState('');
-    const [lastName, setLast] = useState('');
-    const [email, setEmail] = useState('');
-    return(
-        // Main View
-        <View style = {styles.background}>
-
-            <View style = {styles.colorTop}>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirst] = useState("");
+  const [lastName, setLast] = useState("");
+  const [email, setEmail] = useState("");
+  return (
+    // Main View
+    <View style={styles.background}>
+      {/* <View style = {styles.colorTop}>
                 <View style={styles.smallPadding}></View>
                 <Text style={styles.titleText}>Register</Text>
-            </View>
-            <View style = {styles.smallPadding}></View>
-            <Image 
-                style= {{height: 200, width: 200}}
-                source={require('../assets/logo.png')}/>
+            </View> */}
+      <View style={styles.smallPadding}></View>
+      <Image
+        style={{ height: 200, width: 200 }}
+        source={require("../assets/logo.png")}
+      />
 
+      <TextInput
+        style={styles.input}
+        placeholder="First Name"
+        secureTextEntry={false}
+        onChangeText={(newFirst) => setFirst(newFirst)}
+        defaultValue={firstName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Last Name"
+        secureTextEntry={false}
+        onChangeText={(newLast) => setLast(newLast)}
+        defaultValue={lastName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        secureTextEntry={false}
+        onChangeText={(newUsername) => setUsername(newUsername)}
+        defaultValue={username}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        secureTextEntry={false}
+        onChangeText={(newEmail) => setEmail(newEmail)}
+        defaultValue={email}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        onChangeText={(newPassword) => setPassword(newPassword)}
+        defaultValue={password}
+      />
 
-            <TextInput
-                style={styles.input}
-                placeholder='First Name'
-                secureTextEntry={false}  
-                onChangeText={newFirst => setFirst(newFirst)}
-                defaultValue={firstName} 
-            />
-            <TextInput
-                style={styles.input}
-                placeholder='Last Name'
-                secureTextEntry={false}
-                onChangeText={newLast => setLast(newLast)}
-                defaultValue={lastName}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder='Username'
-                secureTextEntry={false}
-                onChangeText={newUsername => setUsername(newUsername)}
-                defaultValue={username}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder='Email'
-                secureTextEntry={false}
-                onChangeText={newEmail => setEmail(newEmail)}
-                defaultValue={email}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder='Password'
-                secureTextEntry={true}
-                onChangeText={newPassword => setPassword(newPassword)}
-                defaultValue={password}
-            />
-            
-            <View style={styles.registerSquare}> 
-                <Button  
-                title='Register Now'
-                color='#fff'
-                onPress={() => [console.log(firstName), console.log(lastName),console.log(username),console.log(email),console.log(password)]}
-                />
-            </View>
+      <View style={styles.registerSquare}>
+        <Button
+          title="Register Now"
+          color="#fff"
+          onPress={() => [
+            console.log(firstName),
+            console.log(lastName),
+            console.log(username),
+            console.log(email),
+            console.log(password),
+          ]}
+        />
+      </View>
 
-            <View style={styles.backSquare}> 
-                <Button  
-                title='Back'
-                color='#fff'
-                onPress={() => console.log("Back")}
-                />
-            </View>
-
-        </View>
-    );
+      <View style={styles.backSquare}>
+        <Button title="Back" color="#fff" onPress={() => console.log("Back")} />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-
-    background: {
-        flex: 1,
-        justifyContent: "flex-start",
-        alignItems: "center",
-        backgroundColor: "white",
-        resizeMode: "contain"
-    },
-    backSquare: {
-        position: "absolute",
-        bottom: "7%",
-        width: "80%",
-        height: "7%",
-        backgroundColor: 'red',
-        justifyContent: 'center',
-    },
-    colorTop: {
-        backgroundColor: "#4FB5D3",
-        width: "100%",
-        height: "15%",
-        borderRadius: 20,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    input: {
-        height: "5%",
-        width: "80%",
-        margin: 12,
-        borderWidth: 1,
-        borderRadius: 10,
-        padding: 10
-    },
-    titleText: {
-        fontWeight: "bold",
-        color: "white",
-        fontSize: 30,
-    },
-    smallPadding: {
-        padding: "2%"
-    },
-    registerSquare: {
-        position: "absolute",
-        bottom: "15%",
-        width: "80%",
-        height: "7%",
-        backgroundColor: '#4FB5D3',
-        justifyContent: 'center',
-
-        },
+  background: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "white",
+    resizeMode: "contain",
+  },
+  backSquare: {
+    position: "absolute",
+    bottom: "7%",
+    width: "80%",
+    height: "7%",
+    backgroundColor: "red",
+    justifyContent: "center",
+  },
+  colorTop: {
+    backgroundColor: "#4FB5D3",
+    width: "100%",
+    height: "15%",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    height: "5%",
+    width: "80%",
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+  titleText: {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 30,
+  },
+  smallPadding: {
+    padding: "2%",
+  },
+  registerSquare: {
+    position: "absolute",
+    bottom: "15%",
+    width: "80%",
+    height: "7%",
+    backgroundColor: "#4FB5D3",
+    justifyContent: "center",
+  },
 });
 
 export default RegisterScreen;
